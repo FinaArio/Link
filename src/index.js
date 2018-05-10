d732448ff492bcc8c5a03982e6199844d0698eb2
Artem <frolkina91mailru@MacBook-Air-Artyom.local>
2018-05-10 19:52:17 +0300

    3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import {syncHistoryWithStore} from 'react-router-redux'
-import {Router, Route, browserHistory} from 'react-router'
+import {Router,Route, browserHistory} from 'react-router'
+
 import {Provider} from 'react-redux'
 
 
@@ -22,30 +23,35 @@ import Login from 'containers/login'
 import SignIp from 'containers/signIn'
 import SignUp from 'containers/sign_up'
 import New_page from 'containers/new_page'
-
+import createHistory from 'history/createBrowserHistory'
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
 ))
 
 
-const history = syncHistoryWithStore(browserHistory, store)
+const history = createHistory()
 
 ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
-                <Route path='/' component={Lending} />
+            <Switch>
+                <Route exact path='/' component={Lending} />
                 <Route path='/trade/:id' component={Coin} />
-                <Route path='/fund' component={Price} />
-                <Route path='/fund/invest-I' component={Calc} /> 
-                <Route path='/fund/invest-II' component={Calc_1} /> 
-                <Route path='/fund/invest-III' component={Calc_3} /> 
-                <Route path='/profile-balance' component={Balance} /> 
+                <Route exact path='/fund' component={Price} />
+                <Route path='/fund/invest-I' component={Calc} />
+                <Route path='/fund/invest-II' component={Calc_1} />
+                <Route path='/fund/invest-III' component={Calc_3} />
+                <Route path='/profile-balance' component={Balance} />
                 <Route path='/news' component={Store} />
                 <Route path='/login' component={Login} />
                 <Route path='/SignIp' component={SignIp} />
                 <Route path='/SignUp' component={SignUp} />
                 <Route path='/news/1' component={New_page} />
+            </Switch>
         </Router>
+        
+
+        
     </Provider>,
     document.getElementById('root')
 );
